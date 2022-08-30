9adb0829e40b33c1789586e25e7fa7087e26050d
Siwei Tang <64385630+tangsiwei0831@users.noreply.github.com>
2022-08-30 14:01:38 -0400

    Update auth-hook.js

--- frontEnd/src/shared/hooks/auth-hook.js
+++ frontEnd/src/shared/hooks/auth-hook.js
@@ -3,7 +3,7 @@ import {useState, useCallback, useEffect} from 'react'
 let logoutTimer;
 
 export const useAuth = () => {
-    const [token, setToken] = useState(false);
+  const [token, setToken] = useState(false);
   const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false)
 
